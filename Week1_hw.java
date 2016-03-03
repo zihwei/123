62e71e841976ef0a79e676004495635b7e51ebcd
zihwei <wei01220123@gmail.com>
2016-03-03 23:45:57 +0800

    week1_hw

---
+++ Week1_hw.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg0303.week1_hw;
+
+/**
+ *
+ * @author 子溦
+ */
+public class Week1_hw {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("《回鄉偶書》 賀知章");
+        System.out.println("少小离家老大回，");
+        System.out.println("鄉音無改鬢毛催。");
+        System.out.println("儿童相見不相識，");
+        System.out.println("笑問客從何處來。");
+        
+
+    }
+    
+}
